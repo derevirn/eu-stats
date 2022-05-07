14e3138dd72c4ba287a78ae557244872af1badd4
Giannis Tolios <derevirn@gmail.com>
2022-05-07 03:26:55 +0300

    update

--- nuts2.py
+++ nuts2.py
@@ -1,3 +1,5 @@
+countries = { "Austria": "AT", "Belgium": "BE", "Bulgaria": "BG", "Croatia": "HR", "Cyprus": "CY", "Czech Republic": "CZ", "Denmark": "DK", "Estonia": "EE", "Finland":"FI", "France": "FR", "Germany": "DE", "Greece": "EL", "Hungary": "HU", "Ireland": "IE", "Italy": "IT", "Latvia": "LV", "Lithuania": "LT", "Luxembourg": "LU", "Malta": "MT", "Netherlands": "NL", "Poland": "PL", "Portugal": "PT", "Romania": "RO", "Slovakia": "SK", "Slovenia": "SI", "Spain": "ES", "Sweden": "SE"}
+
 codes = {'AL': {'AL01': 'Veri',
   'AL02': 'Qender',
   'AL03': 'Jug',
