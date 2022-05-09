249754b72a442fe9be25a7b413a0666c0f4936af
Giannis Tolios <derevirn@gmail.com>
2022-05-09 20:49:41 +0300

    update

--- app.py
+++ app.py
@@ -53,16 +53,22 @@ with plot_container:
     st.markdown(source, unsafe_allow_html = True) 
 
 
-tracking = '''<script type="text/javascript">
+tracking = '''<!-- Default Statcounter code for StatsEuropa
+https://eu-stats.herokuapp.com/ -->
+<script type="text/javascript">
 var sc_project=12751725; 
 var sc_invisible=1; 
 var sc_security="35d8e5a4"; 
 </script>
 <script type="text/javascript"
-src="https://www.statcounter.com/counter/counter.js" async></script>
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
 <noscript><div class="statcounter"><a title="Web Analytics"
-href="https://statcounter.com/" target="_blank"><img class="statcounter"
-src="https://c.statcounter.com/12751725/0/35d8e5a4/1/" alt="Web Analytics"
-referrerPolicy="no-referrer-when-downgrade"></a></div></noscript> '''
+href="https://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="https://c.statcounter.com/12751725/0/35d8e5a4/1/"
+alt="Web Analytics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code --> '''
 
 st.markdown(tracking, unsafe_allow_html = True)
