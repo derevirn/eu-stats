fd165f148b45e5b5fb8f39752708a05ddd8b3fbf
Giannis Tolios <derevirn@gmail.com>
2022-05-09 21:04:24 +0300

    update

--- app.py
+++ app.py
@@ -53,22 +53,22 @@ with plot_container:
     st.markdown(source, unsafe_allow_html = True) 
 
 
-tracking = '''<!-- Default Statcounter code for StatsEuropa
-https://eu-stats.herokuapp.com/ -->
+tracking = '''<!-- Default Statcounter code for StatsEuropa https://eu-stats.herokuapp.com/ -->
+
 <script type="text/javascript">
 var sc_project=12751725; 
-var sc_invisible=0; 
+var sc_invisible=1; 
 var sc_security="35d8e5a4"; 
 </script>
-<script type="text/javascript"
-src="https://www.statcounter.com/counter/counter.js"
-async></script>
-<noscript><div class="statcounter"><a title="Web Analytics"
-href="https://statcounter.com/" target="_blank"><img
-class="statcounter"
-src="https://c.statcounter.com/12751725/0/35d8e5a4/1/"
-alt="Web Analytics"
-referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
-<!-- End of Statcounter Code --> '''
 
-st.markdown(tracking, unsafe_allow_html = True)
+<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
+
+<noscript>
+<div class="statcounter"><a title="Web Analytics" href="https://statcounter.com/" target="_blank">
+<img class="statcounter" src="https://c.statcounter.com/12751725/0/35d8e5a4/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade"></a>
+</div>
+</noscript>
+
+<!-- End of Statcounter Code -->'''
+
+st.components.v1.html(tracking)
